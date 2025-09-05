88ad9605840c1bcdb54150143a070d110249292c
wonderjs <25072556+wonderjs@users.noreply.github.com>
2025-09-05 17:11:49 +0200

    add Demo 1

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,53 @@
+<template>
+  <demo-container>
+    <div class="counter">
+      <counter-button @action="decrement">-</counter-button>
+
+      <counter-value :value="count" />
+
+      <counter-button @action="increment">+</counter-button>
+    </div>
+  </demo-container>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CounterButton from './CounterButton.vue';
+import CounterValue from './CounterValue.vue';
+import DemoContainer from '../components/DemoContainer.vue';
+
+const count = ref(0);
+
+/* Метод уменьшения значения */
+const decrement = () => {
+  count.value = count.value - 1;
+};
+
+/* Метод увеличения значения */
+const increment = () => {
+  count.value = count.value + 1;
+};
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+
+  button {
+    margin: 0 10px;
+    font-size: 1.5rem;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    border: none;
+    background: #dd0000;
+    color: white;
+    cursor: pointer;
+
+    &:hover {
+      background: #ff0000;
+    }
+  }
+}
+</style>
