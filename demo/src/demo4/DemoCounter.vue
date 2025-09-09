65e00cedd5d83bb971a9186ac2c4c81ec387d812
wonderjs <25072556+wonderjs@users.noreply.github.com>
2025-09-09 13:40:20 +0200

    add demo 4

---
+++ demo/src/demo4/DemoCounter.vue
@@ -0,0 +1,48 @@
+<template>
+  <demo-container>
+    <div class="counter">
+      <counter-button
+        :disabled="counterStore.isDecrementDisabled"
+        @action="counterStore.decrement"
+        >-</counter-button
+      >
+
+      <counter-value :value="counterStore.value" />
+
+      <counter-button @action="counterStore.increment">+</counter-button>
+    </div>
+  </demo-container>
+</template>
+
+<script setup>
+import CounterButton from './CounterButton.vue';
+import CounterValue from './CounterValue.vue';
+import DemoContainer from '../components/DemoContainer.vue';
+
+import { useCounterStore } from './store';
+
+const counterStore = useCounterStore();
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+
+  button {
+    margin: 0 10px;
+    font-size: 1.5rem;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    border: none;
+    background: #dd0000;
+    color: white;
+    cursor: pointer;
+
+    &:hover {
+      background: #ff0000;
+    }
+  }
+}
+</style>
--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,12 +1,17 @@
 export default [
   {
-    name: 'Counter Base',
+    name: 'Counter Base 0',
     path: '/counter-0',
     component: () => import('./demo0/DemoCounter.vue'),
   },
   {
-    name: 'Counter Base',
+    name: 'Counter Base 1',
     path: '/counter-1',
     component: () => import('./demo1/DemoCounter.vue'),
+  },
+  {
+    name: 'Counter Base 4',
+    path: '/counter-4',
+    component: () => import('./demo4/DemoCounter.vue'),
   },
 ];
