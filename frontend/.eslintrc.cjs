e000dfcaaf9c56781196b1620812b06c7b22d39a
wonderjs <25072556+wonderjs@users.noreply.github.com>
2025-09-11 17:58:22 +0200

    update eslintrc

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -11,4 +11,9 @@ module.exports = {
   env: {
     'vue/setup-compiler-macros': true,
   },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
 };
